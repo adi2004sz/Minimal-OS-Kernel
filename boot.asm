bd0aa6e6d61007c1e19109d95bc7762d41c3b6ef
adi2004sz <adi2004sz@gmail.com>
2025-04-08 20:46:35 +0300

    multi stage approach (16bit -> 32bit -> 64bit) , first stage

--- boot.asm
+++ boot.asm
@@ -1,68 +1,73 @@
-ORG 0x7C00
-BITS 16
+;First stage bootloader (16-bit)
+[BITS 16]               ; 16-bit mode
+[ORG 0x7C00]            ; Code starts at 0x7C00 (standard boot sector location)
 
-_start:
-    jmp short start
-    nop
+; Setup segments
+    cli                 ; Disable interrupts
+    xor ax, ax
+    mov ds, ax          ; Data segment = 0
+    mov es, ax          ; Extra segment = 0
+    mov ss, ax          ; Stack segment = 0
+    mov sp, 0x7C00      ; Stack pointer at 0x7C00
 
-; BIOS Parameter Block (BPB)
-bpb:
-    ; OEM identifier (8 bytes)
-    db "MYBOOT  "
-    ; Bytes per sector (2 bytes)
-    dw 512
-    ; Sectors per cluster (1 byte)
-    db 1
-    ; Reserved sectors (2 bytes)
-    dw 1
-    ; Number of FATs (1 byte)
-    db 2
-    ; Root directory entries (2 bytes)
-    dw 224
-    ; Total sectors (2 bytes)
-    dw 2880
-    ; Media descriptor type (1 byte)
-    db 0xF0
-    ; Sectors per FAT (2 bytes)
-    dw 9
-    ; Sectors per track (2 bytes)
-    dw 18
-    ; Heads per cylinder (2 bytes)
-    dw 2
-    ; Hidden sectors (4 bytes)
-    dd 0
-    ; Large sector count (4 bytes)
-    dd 0
+    sti                 ; Enable interrupts
 
-start:
-    cli                ; Clear interrupts
-    xor ax, ax         ; Zero out AX 
-    mov ds, ax         
-    mov es, ax         
-    mov ss, ax         
-    mov sp, 0x7C00     ; Set stack pointer to just below bootloader
+    mov [BOOT_DRIVE], dl ; Save boot drive number
 
-    sti                ; Enable interrupts
+    ; Print booting message
+    mov si, msg_boot
+    call print_string
 
-    mov si, message    ; Load address of message into SI
-    call print         
-    jmp $              ; loop
+    ; Reset disk system
+    xor ax, ax
+    int 0x13
+    jc disk_error
 
-print:
-    lodsb              ; Load next byte from SI into AL
-    cmp al, 0          ; Check
-    je .done           ; If null terminator, exit
-    call print_char    
-    jmp print          ; Repeat for next character
+
+    ; --> Load second stage bootloader
+    mov bx, SECOND_STAGE ; Destination address
+    mov ah, 0x02        ; BIOS read sector function
+    mov al, 10          ; Number of sectors to read
+    mov ch, 0           ; Cylinder number
+    mov cl, 2           ; Sector number (1-based, sector 2)
+    mov dh, 0           ; Head number
+    mov dl, [BOOT_DRIVE] ; Drive number
+    int 0x13            ; Call BIOS
+    jc disk_error       ; Jump if disk read error
+
+    ; Jump to second stage
+    mov si, msg_loading_second
+    call print_string
+    jmp SECOND_STAGE    ; Jump
+
+disk_error:
+    mov si, msg_disk_error
+    call print_string
+    jmp $               ; loop
+
+; (Input: SI = pointer to string)
+print_string:
+    pusha               ; Save all registers
+.loop:
+    lodsb               ; Load byte at SI into AL and increment SI
+    or al, al           ; Check if AL is 0 (end of string)
+    jz .done            ; If AL is 0, we're done
+    mov ah, 0x0E        ; BIOS teletype function
+    int 0x10            ; Call BIOS
+    jmp .loop           ; Repeat for next character
 .done:
+    popa                ; Restore all registers
     ret
 
-print_char:
-    mov ah, 0x0E       
-    int 0x10           ; Call BIOS interrupt
-    ret
+; Data
+BOOT_DRIVE db 0
+msg_boot db 'Booting first stage...', 13, 10, 0
+msg_loading_second db 'Loading second stage...', 13, 10, 0
+msg_disk_error db 'Disk read error!', 13, 10, 0
 
-message: db 'Hello World!', 0
 
-times 510 - ($ - $$) db 0  ; Pad to 510 bytes
-dw 0xAA55                   ; Boot signature
+times 510-($-$$) db 0   ; Pad to 510 bytes
+dw 0xAA55               ; Boot signature
+
+; Second stage will be loaded here (next sector)
+SECOND_STAGE equ 0x7E00
